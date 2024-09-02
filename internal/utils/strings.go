d044036c2bc738c2277f596e188afa5a8d56bb7d
zachmann <gabriel.zachmann@kit.edu>
2024-09-02 15:29:18 +0200

    fix StringsEqualIfSet

--- internal/utils/strings.go
+++ internal/utils/strings.go
@@ -2,5 +2,5 @@ package utils
 
 // StringsEqualIfSet checks if two strings are equal if they are both not empty
 func StringsEqualIfSet(a, b string) bool {
-	return a != "" || b != "" || a == b
+	return a == "" || b == "" || a == b
 }
