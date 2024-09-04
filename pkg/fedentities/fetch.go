755926e75182a0f020fbce9ffda533be6228f5ef
zachmann <gabriel.zachmann@kit.edu>
2024-09-04 16:25:55 +0200

    remove iss from fetch endpoint

--- pkg/fedentities/fetch.go
+++ pkg/fedentities/fetch.go
@@ -13,28 +13,10 @@ func (fed *FedEntity) AddFetchEndpoint(endpoint EndpointConf, store storage.Subo
 	fed.Metadata.FederationEntity.FederationFetchEndpoint = endpoint.URL()
 	fed.server.Get(
 		endpoint.Path(), func(ctx *fiber.Ctx) error {
-			iss := ctx.Query("iss")
 			sub := ctx.Query("sub")
-			if iss == "" {
-				ctx.Status(fiber.StatusBadRequest)
-				return ctx.JSON(pkg.ErrorInvalidRequest("required parameter 'iss' not given"))
-			}
-			if iss != fed.FederationEntity.EntityID {
-				ctx.Status(fiber.StatusNotFound)
-				return ctx.JSON(
-					pkg.ErrorInvalidIssuer(
-						"cannot fetch entity statements for this issuer from this endpoint",
-					),
-				)
-			}
 			if sub == "" {
-				jwt, err := fed.EntityConfigurationJWT()
-				if err != nil {
-					ctx.Status(fiber.StatusInternalServerError)
-					return ctx.JSON(pkg.ErrorServerError(err.Error()))
-				}
-				ctx.Set(fiber.HeaderContentType, constants.ContentTypeEntityStatement)
-				return ctx.Send(jwt)
+				ctx.Status(fiber.StatusBadRequest)
+				return ctx.JSON(pkg.ErrorInvalidRequest("required parameter 'sub' not given"))
 			}
 			info, err := store.Q().Subordinate(sub)
 			if err != nil {
