e7512d6c34fc9d0e6590d18a92387c964d860d8e
zachmann <gabriel.zachmann@kit.edu>
2024-09-27 11:25:12 +0200

    use longer code_verifier

--- examples/rp/server.go
+++ examples/rp/server.go
@@ -66,7 +66,7 @@ func handleLogin(w http.ResponseWriter, r *http.Request) {
 		authBuilder = pkg.NewRequestObjectProducer(conf.EntityID, getKey("oidc"), jwa.ES512, 60)
 	}
 	op := r.URL.Query().Get("op")
-	state := randASCIIString(32)
+	state := randASCIIString(64)
 	pkceChallenge := pkce.NewS256PKCE(randASCIIString(20))
 	stateDB[state] = stateData{
 		codeChallange: pkceChallenge,
