147914782146c71c85aa97f823b89653259847b3
zachmann <gabriel.zachmann@kit.edu>
2024-12-16 15:26:02 +0100

    [testbed] improve entitle trust mark

--- examples/edugain-pilot/entitle-trustmarks.sh
+++ examples/edugain-pilot/entitle-trustmarks.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+docker exec edugain-pilot-edugain-1 /tacli -c /data/config.yaml tm add https://edugain.org/member https://edugain.fedservice.lh
+
+docker exec edugain-pilot-garr-1 /tacli -c /data/config.yaml tm add https://refeds.org/sirtfi https://garr.fedservice.lh
+
 docker exec edugain-pilot-puhuri-1 /tacli -c /data/config.yaml tm add https://puhuri.io https://puhuri.fedservice.lh
 docker exec edugain-pilot-puhuri-1 /tacli -c /data/config.yaml tm add https://puhuri.io https://haka.fedservice.lh
 docker exec edugain-pilot-puhuri-1 /tacli -c /data/config.yaml tm add https://puhuri.io https://surf-rp.fedservice.lh
