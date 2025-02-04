a6412f972e4ae892dcb02a689d6cc62eea9e198a
zachmann <gabriel.zachmann@kit.edu>
2025-02-04 10:35:31 +0100

    define additional application types and jwt typs

--- pkg/constants/constants.go
+++ pkg/constants/constants.go
@@ -2,13 +2,19 @@ package constants
 
 // Constants for JWT Types
 const (
-	ContentTypeEntityStatement = "application/entity-statement+jwt"
-	ContentTypeResolveResponse = "application/resolve-response+jwt"
-	ContentTypeTrustMark       = "application/trust-mark+jwt"
-	JWTTypeEntityStatement     = "entity-statement+jwt"
-	JWTTypeTrustMarkDelegation = "trust-mark-delegation+jwt"
-	JWTTypeTrustMark           = "trust-mark+jwt"
-	JWTTypeResolveResponse     = "resolve-response+jwt"
+	ContentTypeEntityStatement              = "application/entity-statement+jwt"
+	ContentTypeTrustMark                    = "application/trust-mark+jwt"
+	ContentTypeResolveResponse              = "application/resolve-response+jwt"
+	ContentTypeTrustChain                   = "application/trust-chain+json"
+	ContentTypeTrustMarkDelegation          = "application/trust-mark-delegation+jwt"
+	ContentTypeJWKS                         = "application/jwk-set+jwt"
+	ContentTypeExplicitRegistrationResponse = "application/explicit-registration-response+jwt"
+	JWTTypeEntityStatement                  = "entity-statement+jwt"
+	JWTTypeTrustMarkDelegation              = "trust-mark-delegation+jwt"
+	JWTTypeTrustMark                        = "trust-mark+jwt"
+	JWTTypeResolveResponse                  = "resolve-response+jwt"
+	JWTTypeJWKS                             = "jwk-set+jwt"
+	JWTTypeExplicitRegistrationResponse     = "explicit-registration-response+jwt"
 )
 
 // Constants for entity types
